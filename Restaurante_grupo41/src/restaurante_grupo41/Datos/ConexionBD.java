b4e0dffafd9582823f779047fc78fa57e0b4a670
PabloASchamber <pablo.schamber@hotmail.com>
2023-10-02 17:50:46 -0300

    consigna

---
+++ Restaurante_grupo41/src/restaurante_grupo41/Datos/ConexionBD.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package restaurante_grupo41.Datos;
+
+/**
+ *
+ * @author Pablo
+ */
+public class ConexionBD {
+    
+}
