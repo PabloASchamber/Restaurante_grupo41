fa8d97cac4f96ed86e7880f1f71e34b3c6d4eb69
PabloASchamber <pablo.schamber@hotmail.com>
2023-10-03 09:26:11 -0300

    conexion

--- Restaurante_grupo41/src/restaurante_grupo41/Datos/ConexionBD.java
+++ Restaurante_grupo41/src/restaurante_grupo41/Datos/ConexionBD.java
@@ -1,13 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package restaurante_grupo41.Datos;
 
-/**
- *
- * @author Pablo
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class ConexionBD {
+       private static final String bd = "restaurante";
+       private static final String url = "jdbc:mariadb://localhost:3306/";
+       private static final String usuario = "root";
+       private static final String pass = "";
+       private static final String driver = "org.mariadb.jdbc.Driver";
+       private static Connection cx;
+    
+
+    private ConexionBD() {
+    }
+
+    public static Connection conectar() {
+        if (cx==null){
+        try {
+            
+            Class.forName(driver);
+            cx = DriverManager.getConnection(url + bd, usuario, pass);
+            System.out.println("conexion exitosa");
+
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println("error en la conexion");
+            Logger.getLogger(ConexionBD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+        return cx;
+    }
+    
     
 }
--- Restaurante_grupo41/src/restaurante_grupo41/Restaurante_grupo41.java
+++ Restaurante_grupo41/src/restaurante_grupo41/Restaurante_grupo41.java
@@ -1,9 +1,13 @@
 
 package restaurante_grupo41;
 
+import java.sql.Connection;
+import restaurante_grupo41.Datos.ConexionBD;
+
 public class Restaurante_grupo41 {
 
     public static void main(String[] args) {
+        Connection cbd = ConexionBD.conectar();
         
         
     }
