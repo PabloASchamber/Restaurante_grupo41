38bc8cc053d161734898f2eb8d0516103f62dc27
gabrielbellagamba <bellagambagabriel@gmail.com>
2023-10-31 15:58:53 -0300

    Try Catch mesaData

--- Restaurante_grupo41/src/restaurante_grupo41/Datos/MeseroData.java
+++ Restaurante_grupo41/src/restaurante_grupo41/Datos/MeseroData.java
@@ -24,28 +24,31 @@ public class MeseroData {
         String sql = "INSERT INTO mesero( nombre, password, usuario, administrador) VALUES (?,?,?,?)";
 
         try {
-            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            ps.setString(1, mesero.getNombre());
-            ps.setString(2, mesero.getPass()); //hacerle el mismo password
-            ps.setString(3, mesero.getUsuario());
-            ps.setBoolean(4, false);
-            ps.executeUpdate();
+            try {
+                PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+                ps.setString(1, mesero.getNombre());
+                ps.setString(2, mesero.getPass()); //hacerle el mismo password
+                ps.setString(3, mesero.getUsuario());
+                ps.setBoolean(4, false);
+                ps.executeUpdate();
 
-            ResultSet rs = ps.getGeneratedKeys();
+                ResultSet rs = ps.getGeneratedKeys();
 
-            if (rs.next()) {
+                if (rs.next()) {
 
-                mesero.setNombre(rs.getString("insert_id"));
-                mesero.setPass(rs.getString("insert_id"));
-                mesero.setUsuario(rs.getString("insert_id"));
-                mesero.setAdministrador(false);
-                mesero.setIdMesero(rs.getInt("insert_id"));
+                    mesero.setNombre(rs.getString("insert_id"));
+                    mesero.setPass(rs.getString("insert_id"));
+                    mesero.setUsuario(rs.getString("insert_id"));
+                    mesero.setAdministrador(false);
+                    mesero.setIdMesero(rs.getInt("insert_id"));
 
-                JOptionPane.showMessageDialog(null, "mesero agregado correctamente");
+                    JOptionPane.showMessageDialog(null, "mesero agregado correctamente");
+                }
+
+                ps.close();
+            } catch (java.sql.SQLIntegrityConstraintViolationException ex2) {
+                JOptionPane.showMessageDialog(null, " Usuario existente ");
             }
-
-            ps.close();
-
         } catch (SQLException ex) {
             Logger.getLogger(MeseroData.class.getName()).log(Level.SEVERE, null, ex);
 
@@ -58,18 +61,21 @@ public class MeseroData {
         String sql = "UPDATE mesero SET nombre =? ,password=?, usuario=?  WHERE idMesero= ? ";
 
         try {
-            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            try {
+                PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
-            ps.setString(1, mesero.getNombre());
-            ps.setString(2, mesero.getPass());
-            ps.setString(3, mesero.getUsuario());
-            ps.setInt(4, mesero.getIdMesero());
-            int exito = ps.executeUpdate();
-            if (exito == 1) {
-                JOptionPane.showMessageDialog(null, "mesero modificado");
+                ps.setString(1, mesero.getNombre());
+                ps.setString(2, mesero.getPass());
+                ps.setString(3, mesero.getUsuario());
+                ps.setInt(4, mesero.getIdMesero());
+                int exito = ps.executeUpdate();
+                if (exito == 1) {
+                    JOptionPane.showMessageDialog(null, "mesero modificado");
 
+                }
+            } catch (java.sql.SQLIntegrityConstraintViolationException ex2) {
+                JOptionPane.showMessageDialog(null, " Usuario existente ");
             }
-
         } catch (SQLException ex) {
             Logger.getLogger(MeseroData.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -181,27 +187,26 @@ public class MeseroData {
 
                 String sqluc = "SELECT * FROM mesero WHERE usuario= BINARY ? && password= BINARY ?";
 
-                    PreparedStatement ps = con.prepareStatement(sqluc, Statement.RETURN_GENERATED_KEYS);
+                PreparedStatement ps = con.prepareStatement(sqluc, Statement.RETURN_GENERATED_KEYS);
 
-                    ps.setString(1, mesero.getUsuario());
-                    ps.setString(2, mesero.getPass());
+                ps.setString(1, mesero.getUsuario());
+                ps.setString(2, mesero.getPass());
 
-                    ResultSet rs = ps.executeQuery();
+                ResultSet rs = ps.executeQuery();
 
-                    if (rs.next()) {
-                        ingreso.setIdMesero(rs.getInt("idmesero"));
-                        ingreso.setNombre(rs.getNString("nombre"));
-                        ingreso.setUsuario(mesero.getUsuario());
-                        ingreso.setPass(mesero.getPass());
-                        ingreso.setAdministrador(rs.getBoolean("administrador"));
-                        return ingreso;
+                if (rs.next()) {
+                    ingreso.setIdMesero(rs.getInt("idmesero"));
+                    ingreso.setNombre(rs.getNString("nombre"));
+                    ingreso.setUsuario(mesero.getUsuario());
+                    ingreso.setPass(mesero.getPass());
+                    ingreso.setAdministrador(rs.getBoolean("administrador"));
+                    return ingreso;
 
-                    } else {
-                        JOptionPane.showMessageDialog(null, "contraseña incorrecta");
-                        return null;
-                    }
+                } else {
+                    JOptionPane.showMessageDialog(null, "contraseña incorrecta");
+                    return null;
+                }
 
-                
             } else {
                 JOptionPane.showMessageDialog(null, "usuario no encontrado");
                 return null;
@@ -211,17 +216,17 @@ public class MeseroData {
         }
 
         return null;
-        
+
     }
 
-    public ArrayList <Mesero> ListaMesero (){
-     ArrayList <Mesero> listam= new ArrayList <> (); 
-           String sqlLista="select * from mesero";
+    public ArrayList<Mesero> ListaMesero() {
+        ArrayList<Mesero> listam = new ArrayList<>();
+        String sqlLista = "select * from mesero";
         try {
-            PreparedStatement ps=con.prepareStatement(sqlLista);
-            ResultSet rs =ps.executeQuery();
-            while (rs.next()){
-                Mesero mesero=new Mesero();
+            PreparedStatement ps = con.prepareStatement(sqlLista);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Mesero mesero = new Mesero();
                 mesero.setNombre(rs.getString("nombre"));
                 mesero.setUsuario(rs.getString("usuario"));
                 mesero.setPass(rs.getString("password"));
@@ -229,14 +234,12 @@ public class MeseroData {
                 mesero.setIdMesero(rs.getInt("idmesero"));
                 listam.add(mesero);
             }
-            
+
         } catch (SQLException ex) {
             Logger.getLogger(MesaData.class.getName()).log(Level.SEVERE, null, ex);
         }
-     
-     return listam;
+
+        return listam;
     }
-    
-    
-    
+
 }//fin
